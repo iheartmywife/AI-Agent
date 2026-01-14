110d44a987b50e8722b6ea93ba3418f4749272f9
iheartmywife <kylejchermak@gmail.com>
2026-01-14 07:21:15 -0800

    added file info reading

---
+++ functions/get_files_info.py
@@ -0,0 +1,20 @@
+import os
+
+def get_files_info(working_directory, directory="."):
+    working_dir_abs = os.path.abspath(working_directory)
+    target_dir = os.path.normpath(os.path.join(working_dir_abs, directory))
+    # Will be True or False
+    valid_target_dir = os.path.commonpath([working_dir_abs, target_dir]) == working_dir_abs
+    if not valid_target_dir:
+        return f'Error: Cannot list "{directory}" as it is outside the permitted working directory'
+    if not os.path.isdir(target_dir):
+        return f'Error: "{directory}" is not a directory'
+    
+    return_string = ""
+    try:
+        for item in os.listdir(target_dir):
+            full_item_path = os.path.join(target_dir, item)
+            return_string += f'- {item}: file_size={os.path.getsize(full_item_path)} bytes, is_dir={os.path.isdir(full_item_path)}\n'
+        return return_string
+    except Exception as e:
+        return f'Error: {e}'
--- main.py
+++ main.py
@@ -1,5 +1,36 @@
+import os
+import argparse
+from dotenv import load_dotenv
+from google import genai
+from google.genai import types
+
 def main():
+    parser = argparse.ArgumentParser(description="Chatbot")
+    parser.add_argument("user_prompt", type=str, help="User prompt")
+    parser.add_argument("--verbose", action="store_true", help="Enable verbose output")
+    args = parser.parse_args()
+
+    load_dotenv()
+    api_key = os.environ.get("GEMINI_API_KEY")
+    if not api_key:
+        raise RuntimeError("GEMINI_API_KEY environment variable not set")
+
+    client = genai.Client(api_key=api_key)
+    messages = [types.Content(role="user", parts=[types.Part(text=args.user_prompt)])]
+
     print("Hello from aiagent!")
+    try:
+        response = client.models.generate_content(
+        model='gemini-2.5-flash', contents= messages
+        )
+        if args.verbose and response.usage_metadata is not None:
+            print(f'User prompt: {args.user_prompt}')
+            print(f'Prompt tokens: {response.usage_metadata.prompt_token_count}')
+            print(f'Response tokens: {response.usage_metadata.candidates_token_count}')
+        print(response.text)
+    except Exception as e:
+        print(f"Error: {e}")
+        return
 
 
 if __name__ == "__main__":
